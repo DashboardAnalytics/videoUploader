08aee3220116337f0d62542f5e04d16e65f71b7a
Edgar Blau <edgar.blau@usach.cl>
2019-10-02 13:29:09 -0600

    Added API connection.

--- APIConsumer.py
+++ APIConsumer.py
@@ -2,16 +2,16 @@ import json
 import requests
 import base64
 
-baseUrl = 'http://127.0.0.1:8080/controlTables/'
+baseUrl = 'http://127.0.0.1:3000/control-tables/'
 
 apikey = 'C020D7AD82F2F7F1476A8E95D95A39AA34E714EB8277539AF0D8011E1E8CB18D850B5D8DC4678F6E217271D398E635AA119F7FB88F55199A8C3CB4D628CA3DDA';
 
-def createVideoData(videoData):
+def createVideoData(filename, videoNumber, store, shoppingCenter):
 
-    response = requests.post(baseUrl + 'create', data = videoData)
+    videoData = {"video_name": filename, "status": 1, "video_number": videoNumber, "store": store, "shopping_center": shoppingCenter}
+    response = requests.post(baseUrl, data = videoData)
 
     data = response.json()
-
     return data
 
 def getVideoDataByName(videoName):

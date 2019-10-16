4e6cdd95c0569fe2a76fe05b15e61c427d89353b
Edgar Blau <edgar.blau@usach.cl>
2019-10-16 11:43:50 -0600

    Fixed API url.

--- APIConsumer.py
+++ APIConsumer.py
@@ -2,7 +2,7 @@ import json
 import requests
 import base64
 import time
-baseUrl = 'http://127.0.0.1:3000/control-tables/'
+baseUrl = 'http://35.193.208.246:3000/control-tables/'
 
 apikey = 'C020D7AD82F2F7F1476A8E95D95A39AA34E714EB8277539AF0D8011E1E8CB18D850B5D8DC4678F6E217271D398E635AA119F7FB88F55199A8C3CB4D628CA3DDA';
 
